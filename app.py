d1ec9d34f25230659258eb17e0052f15e9f54614
Álvaro Cafe <alvaro.campos.ferreira@gmail.com>
2021-05-17 20:25:11 -0300

    Adicionando script principal.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-# PF003
+'''
+PF003
 
 Objetivos:
 - Facilitar pesquisa de conteúdos de iniciativas de lei na Câmara dos Deputados e no Senado Federal
 
 Os sites a serem pesquisados são: https://www.camara.leg.br/ e https://www12.senado.leg.br/hpsenado
+'''
 
-Projeto proposto por um dos estudantes da disciplina no primeiro semestre de 2021.
